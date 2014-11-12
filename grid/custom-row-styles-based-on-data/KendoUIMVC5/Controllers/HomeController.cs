5233cb7a874314041c2a5405c226793bc314522e
Petur Subev <petur.subev@gmail.com>
2014-11-12 16:32:05 +0200

    Add readme

---
+++ grid/custom-row-styles-based-on-data/KendoUIMVC5/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using KendoUIMVC5.ViewModels;
+using KendoUIMVC5;
+
+namespace CustomStylesOnDataBound.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Employees_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            Northwind db = new Northwind();
+            var employees = db.Employees.Select(x => new EmployeeViewModel()
+            {
+                EmployeeId = x.EmployeeID,
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+                Address = x.Address,
+            });
+
+            return Json(employees.ToDataSourceResult(request));
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ grid/custom-row-styles-based-on-data/KendoUIMVC5/Models/EmployeeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KendoUIMVC5.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+    }
+}
