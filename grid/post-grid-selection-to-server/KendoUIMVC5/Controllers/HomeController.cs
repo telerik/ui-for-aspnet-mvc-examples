42002b59dbdfd68aef9539d93c11b46062c1cdc9
Petur Subev <petur.subev@gmail.com>
2014-11-13 17:19:17 +0200

    Add post-grid-selection-to-server article

---
+++ grid/post-grid-selection-to-server/KendoUIMVC5/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using KendoUIMVC5;
+
+namespace CustomStylesOnDataBound.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(int[] productID)
+        {
+            using (var northwind = new Northwind())
+            {
+                if (productID != null)
+                {
+                    var selectedProducts = (from product in northwind.Products
+                                            where productID.Contains(product.ProductID)
+                                            select product).ToList();
+
+                    ViewData["SelectedProducts"] = selectedProducts;
+                }
+
+                return View();
+            }
+        }
+
+        public ActionResult Read([DataSourceRequest]DataSourceRequest request)
+        {
+            using (var northwind = new Northwind())
+            {
+                return Json(northwind.Products.ToDataSourceResult(request, select=> new {select.ProductID, select.ProductName, select.UnitPrice, select.UnitsInStock}));
+            }
+        }
+    }
+}
---
+++ grid/post-grid-selection-to-server/KendoUIMVC5/Northwind.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\subev\Downloads\KendoUIMVC5EF6\KendoUIMVC5\Northwind.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
