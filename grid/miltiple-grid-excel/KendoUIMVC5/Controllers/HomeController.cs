54b2a2d9118b4d6487b99e9052b16a357d22189b
Atanas Korchev <akorchev@gmail.com>
2014-11-27 17:42:25 +0200

    Add multiple grid excel export demo.

---
+++ grid/miltiple-grid-excel/KendoUIMVC5/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using KendoUIMVC5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KendoUIMVC5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult Orders_Read([DataSourceRequest]DataSourceRequest request)
+        {
+            using (var northwind = new NorthwindEntities())
+            {
+                return Json(northwind.Orders.ToDataSourceResult(request, o => new
+                    {
+                        o.OrderID,
+                        o.Freight,
+                        o.OrderDate,
+                        o.ShipName,
+                        o.ShipCity
+                    }));
+            }
+        }
+
+        public ActionResult Products_Read([DataSourceRequest]DataSourceRequest request)
+        {
+            using (var northwind = new NorthwindEntities())
+            {
+                return Json(northwind.Products.ToDataSourceResult(request, p => new
+                {
+                    p.ProductID,
+                    p.ProductName,
+                    p.QuantityPerUnit,
+                    p.UnitPrice,
+                    p.UnitsInStock
+                }));
+            }
+        }
+    }
+}
