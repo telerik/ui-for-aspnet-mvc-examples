9e3ab4c8c3e47b0f55a77435133279be96cbf131
Atanas Korchev <akorchev@gmail.com>
2014-11-27 16:25:08 +0200

    Add alt rows example

---
+++ grid/alternating-rows/KendoUIMVC5/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using KendoUIMVC5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KendoUIMVC5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult Orders_Read([DataSourceRequest]DataSourceRequest request)
+        {
+            using (var northwind = new NorthwindEntities())
+            {
+                return Json(northwind.Orders.ToDataSourceResult(request, o => new
+                    {
+                        o.OrderID,
+                        o.Freight,
+                        o.OrderDate,
+                        o.ShipName,
+                        o.ShipCity
+                    }));
+            }
+        }
+
+    }
+}
