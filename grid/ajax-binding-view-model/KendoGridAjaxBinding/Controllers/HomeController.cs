b5c399627ac682b82ab996e9038d7f19eb201906
Atanas Korchev <akorchev@gmail.com>
2014-11-28 11:39:46 +0200

    Add ajax binding view model project.

---
+++ grid/ajax-binding-view-model/KendoGridAjaxBinding/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using KendoGridAjaxBinding.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KendoGridAjaxBinding.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult Products_Read([DataSourceRequest]DataSourceRequest request)
+        {
+            using (var northwind = new NorthwindEntities())
+            {
+                IQueryable<Product> products = northwind.Products;
+                // Convert the Product entities to ProductViewModel instances
+                DataSourceResult result = products.ToDataSourceResult(request, product => new ProductViewModel
+                {
+                    ProductID = product.ProductID,
+                    ProductName = product.ProductName,
+                    UnitsInStock = product.UnitsInStock
+                });
+                return Json(result);
+            }
+        }
+    }
+}
---
+++ grid/ajax-binding-view-model/KendoGridAjaxBinding/Models/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KendoGridAjaxBinding.Models
+{
+    public class ProductViewModel
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public short? UnitsInStock { get; set; }
+    }
+}
