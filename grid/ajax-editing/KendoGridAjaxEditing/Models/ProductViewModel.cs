fe395f03bfd6f38ceed63f7557997e3fb5dfa5d9
Atanas Korchev <akorchev@gmail.com>
2014-11-28 13:48:38 +0200

    Add ajax editing project.

---
+++ grid/ajax-editing/KendoGridAjaxEditing/Models/ProductViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KendoGridAjaxEditing.Models
+{
+    public class ProductViewModel
+    {
+        public int ProductID { get; set; }
+        // The ProductName property is required
+        [Required]
+        public string ProductName { get; set; }
+        // Use the Integer editor template for the UnitsInStock property
+        [UIHint("Integer")]
+        public short? UnitsInStock { get; set; }
+    }
+}
