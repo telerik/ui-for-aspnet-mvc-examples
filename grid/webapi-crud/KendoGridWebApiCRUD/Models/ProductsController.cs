fcd8900292e8376117769760b4a3ee563dcd321b
Atanas Korchev <akorchev@gmail.com>
2014-12-01 18:43:39 +0200

    Add WebAPI CRUD project.

---
+++ grid/webapi-crud/KendoGridWebApiCRUD/Models/ProductsController.cs
@@ -0,0 +1,117 @@
+﻿using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace KendoGridWebApiCRUD.Models
+{
+    public class ProductsController : ApiController
+    {
+        private NorthwindEntities db = new NorthwindEntities();
+
+        // GET api/Products
+        public DataSourceResult GetProducts([System.Web.Http.ModelBinding.ModelBinder(typeof(WebApiDataSourceRequestModelBinder))]DataSourceRequest request)
+        {
+            return db.Products.ToDataSourceResult(request);
+        }
+
+        // GET api/Products/5
+        public Product GetProduct(int id)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return product;
+        }
+
+        // PUT api/Products/5
+        public HttpResponseMessage PutProduct(int id, Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+            if (id != product.ProductID)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            db.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        // POST api/Products
+        public HttpResponseMessage PostProduct(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+
+                DataSourceResult result = new DataSourceResult
+                {
+                    Data = new[] { product },
+                    Total = 1
+                };
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, result);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = product.ProductID }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+
+        // DELETE api/Products/5
+        public HttpResponseMessage DeleteProduct(int id)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Products.Remove(product);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, product);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
