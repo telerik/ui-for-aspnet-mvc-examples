a0f12192993ee46bc69e6ea3e14aad9fdf744413
Atanas Korchev <akorchev@gmail.com>
2014-12-01 17:29:57 +0200

    Add server binding demo.

---
+++ grid/server-binding/KendoGridServerBinding/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using KendoGridServerBinding.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KendoGridServerBinding.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var northwind = new NorthwindEntities();
+            var products = northwind.Products;
+            return View(products);
+        }
+    }
+}
