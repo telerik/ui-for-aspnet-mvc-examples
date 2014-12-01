d648284812d285db8f814132afc8e5edcee4ea41
Atanas Korchev <akorchev@gmail.com>
2014-12-01 18:22:28 +0200

    Add server detail template.

---
+++ grid/server-detail-template/KendoGridServerDetailTemplate/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using KendoGridServerDetailTemplate.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KendoGridServerDetailTemplate.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var northwind = new NorthwindEntities();
+            ViewBag.Products = northwind.Products;
+            return View();
+        }
+
+    }
+}
---
+++ grid/server-detail-template/KendoGridServerDetailTemplate/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KendoGridServerDetailTemplate.MvcApplication" Language="C#" %>
