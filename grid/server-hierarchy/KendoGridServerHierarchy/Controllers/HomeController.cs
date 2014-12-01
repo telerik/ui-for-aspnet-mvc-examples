c3625d76eda8118e48979c4ab89d4d39d26acda8
Atanas Korchev <akorchev@gmail.com>
2014-12-01 18:28:46 +0200

    Add server hierarchy demo.

---
+++ grid/server-hierarchy/KendoGridServerHierarchy/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using KendoGridServerHierarchy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KendoGridServerHierarchy.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var northwind = new NorthwindEntities();
+            ViewBag.Categories = northwind.Categories;
+            return View();
+        }
+    }
+}
---
+++ grid/server-hierarchy/KendoGridServerHierarchy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KendoGridServerHierarchy.MvcApplication" Language="C#" %>
