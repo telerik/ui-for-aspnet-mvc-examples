97657bae035fb5a46dd5313a9009d20829781b96
Vladimir Iliev <vladimirivanoviliev@gmail.com>
2014-12-15 16:35:24 +0200

    Add Preserve toolabar template of the Grid demo.

---
+++ grid/grid-preserve-server-toolbar-template-after-set-options/ForeignKeyColumnDemo/Models/Order.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ForeignKeyColumnDemo.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+
+        private DateTime orderDate;
+
+        //keep UTC Dates
+        public DateTime OrderDate
+        {
+            get
+            {
+                return orderDate;
+            }
+            set
+            {
+                this.orderDate = new DateTime(value.Ticks, DateTimeKind.Utc);
+            }
+        }
+
+        public string OrderDescription { get; set; }
+        public int EmployeeID { get; set; }
+
+        [UIHint("Currency")]
+        public decimal OrderTotal { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ grid/grid-preserve-server-toolbar-template-after-set-options/README.md
@@ -0,0 +1,3 @@
+#Preserve the Grid server toolbar template after using the "setOptions" method
+
+This project demonstrates how to preserve the Telerik UI for ASP.NET MVC Grid server toolbar template after using the "setOptions" method.
