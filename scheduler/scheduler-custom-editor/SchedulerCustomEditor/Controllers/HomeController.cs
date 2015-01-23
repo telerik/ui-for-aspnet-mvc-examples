d7a2cecd32bdbc8ab838f14158f52bf1a0abec20
George Krystev <ggkrustev@yahoo.com>
2015-01-23 19:19:17 +0200

    Add MVC Scheduler custom editor demo

---
+++ scheduler/scheduler-custom-editor/SchedulerCustomEditor/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿namespace SchedulerCustomEditor.Controllers
+{
+    using Kendo.Mvc.Extensions;
+    using Kendo.Mvc.UI;
+    using SchedulerCustomEditor.Models;
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        private SchedulerMeetingService meetingService;
+
+
+        public HomeController()
+        {
+            this.meetingService = new SchedulerMeetingService();
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View(new MeetingViewModel());
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public virtual JsonResult Meetings_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            return Json(meetingService.GetAll().ToDataSourceResult(request));
+        }
+
+        public virtual JsonResult Meetings_Destroy([DataSourceRequest] DataSourceRequest request, MeetingViewModel meeting)
+        {
+            if (ModelState.IsValid)
+            {
+                meetingService.Delete(meeting, ModelState);
+            }
+
+            return Json(new[] { meeting }.ToDataSourceResult(request, ModelState));
+        }
+
+        public virtual JsonResult Meetings_Create([DataSourceRequest] DataSourceRequest request, MeetingViewModel meeting)
+        {
+            if (ModelState.IsValid)
+            {
+                meetingService.Insert(meeting, ModelState);
+            }
+
+            return Json(new[] { meeting }.ToDataSourceResult(request, ModelState));
+        }
+
+        public virtual JsonResult Meetings_Update([DataSourceRequest] DataSourceRequest request, MeetingViewModel meeting)
+        {
+            if (ModelState.IsValid)
+            {
+                meetingService.Update(meeting, ModelState);
+            }
+
+            return Json(new[] { meeting }.ToDataSourceResult(request, ModelState));
+        }
+    }
+}
