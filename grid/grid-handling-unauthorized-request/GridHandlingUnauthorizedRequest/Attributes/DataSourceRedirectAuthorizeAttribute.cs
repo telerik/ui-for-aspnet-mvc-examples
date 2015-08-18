37a5f473285d0613622e8b30db0c284958e3c2e2
Daniel <karada@abv.bg>
2015-08-18 12:08:19 +0300

    Add example that demonstrates handling unauthorized requests

---
+++ grid/grid-handling-unauthorized-request/GridHandlingUnauthorizedRequest/Attributes/DataSourceRedirectAuthorizeAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kendo.Mvc.UI;
+
+namespace GridHandlingUnauthorizedRequest.Attributes
+{
+    public class DataSourceRedirectAuthorizeAttribute : AuthorizeAttribute
+    {
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {            
+            if (filterContext.HttpContext.Request.IsAjaxRequest())
+            {
+                UrlHelper urlHelper = new UrlHelper(filterContext.RequestContext);
+                filterContext.Result = new JsonResult
+                {
+                    Data = new DataSourceResult { Errors = new { RedirectUrl = urlHelper.Action("Login", "Account") } }
+                };
+            }
+            else
+            {
+                base.HandleUnauthorizedRequest(filterContext);
+            }
+        }
+    }
+}
---
+++ grid/grid-handling-unauthorized-request/GridHandlingUnauthorizedRequest/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GridHandlingUnauthorizedRequest.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+    }
+}
