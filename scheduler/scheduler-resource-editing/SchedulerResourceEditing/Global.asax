6055a0bd8fd9e1b5978a0d6c6260f7c8837c12b3
Vladimir Iliev <vladimirivanoviliev@gmail.com>
2015-10-07 15:05:12 +0300

    Add Scheduler resource editing example

---
+++ scheduler/scheduler-resource-editing/SchedulerResourceEditing/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SchedulerResourceEditing.MvcApplication" Language="C#" %>
---
+++ scheduler/scheduler-resource-editing/SchedulerResourceEditing/Models/Attendie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchedulerResourceEditing.Models
+{
+    public class Attendee
+    {
+        public string Text { get; set; }
+        public int Value { get; set; }
+        public string Color { get; set; }
+    }
+}
