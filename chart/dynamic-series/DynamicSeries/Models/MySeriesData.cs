a235f9f88a82bb12bfd31dcb8b936dfecc43c840
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2015-11-27 11:04:55 +0200

    Add Chart Dynamic Series demo

---
+++ chart/dynamic-series/DynamicSeries/Models/MySeriesData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DynamicSeries.Models
+{
+    public class MySeriesData
+    {
+        public string Name { get; set;  }
+        public string Stack { get; set;  }
+        public IEnumerable<decimal> Data { get; set;  }
+    }
+}
---
+++ chart/dynamic-series/DynamicSeries/Models/MyViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DynamicSeries.Models
+{
+    public class MyViewModel
+    {
+        public MyViewModel()
+        {
+            Series = new List<MySeriesData>();
+            Categories = new List<string>();
+        }
+
+        public List<MySeriesData> Series
+        {
+            get;
+            private set;
+        }
+
+        public List<string> Categories
+        {
+            get;
+            private set;
+        }
+    }
+}
