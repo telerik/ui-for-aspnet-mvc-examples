b8927413ffd7c390536c13aef9087137cf042b4c
ivan.danchev <ifujin@gmail.com>
2016-08-12 10:29:29 +0300

    Add GridAjaxHierarchyInCellEditing and GridAjaxHierarchyEditingWithCheckBoxes

---
+++ grid/grid-ajax-hierarchy-incell-edititng/AjaxHierarchyEditing/Models/OrderViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AjaxHierarchyEditing.Models
+{
+    public class OrderViewModel
+    {
+        public int OrderID { get; set; }
+
+        public string ShipCountry { get; set; }
+
+        public string ShipAddress { get; set; }
+
+        public string ShipName { get; set; }
+
+        public DateTime? ShippedDate { get; set; }
+    }
+}
