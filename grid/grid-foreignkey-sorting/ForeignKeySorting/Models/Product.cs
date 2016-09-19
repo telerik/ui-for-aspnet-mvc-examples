c7644e43477d97ba4575550d51cbec6734ce15cc
hadjieva <hadjieva@hadjieva.bedford.progress.com>
2016-09-19 07:20:44 +0300

    Added ForeignKeySorting and ForeignKeyColumnCRUD grid examples

---
+++ grid/grid-foreignkey-sorting/ForeignKeySorting/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ForeignKeySorting.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
