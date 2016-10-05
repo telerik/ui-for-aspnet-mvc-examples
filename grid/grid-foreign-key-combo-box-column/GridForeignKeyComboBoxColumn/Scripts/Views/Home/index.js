ac7aa9d4f25d5f34a11df909892e2c3e881fc722
jfernandeze <karru0@gmail.com>
2016-10-05 11:41:53 +0200

    When is select the first item of ComboBox and you pass to edit mode the combobox no continious the the select 0, for this you need to change the select and return this

---
+++ grid/grid-foreign-key-combo-box-column/GridForeignKeyComboBoxColumn/Scripts/Views/Home/index.js
@@ -0,0 +1,11 @@
+﻿function changeEditInline(e) {
+    if (e.model.isNew() === false) {
+        
+        var kendoComboBoxEmployee = $("#EmployeeId").data("kendoComboBox");
+
+        if (kendoComboBoxEmployee.select() === 0) {
+            kendoComboBoxEmployee.select(-1);
+            kendoComboBoxEmployee.select(0);
+        }        
+    }
+}
