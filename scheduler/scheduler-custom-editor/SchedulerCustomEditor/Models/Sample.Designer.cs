06bdf6a17abb775f953f87122246928d408a5d25
Nencho Nenchev <nenchef@gmail.com>
2016-11-03 18:02:41 +0200

    updated Scheduler Custom Editor example to use IList instead of IQuarable, due to code refactoring

--- scheduler/scheduler-custom-editor/SchedulerCustomEditor/Models/Sample.Designer.cs
+++ scheduler/scheduler-custom-editor/SchedulerCustomEditor/Models/Sample.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\Work\ui-for-aspnet-mvc-examples\scheduler\scheduler-custom-editor\SchedulerCustomEditor\Models\Sample.edmx'. 
+﻿// Default code generation is disabled for model 'C:\Users\viliev\Desktop\CodeLibraries\SchedulerCustomEditor_OFFICIAL_RTM\SchedulerCustomEditor\Models\Sample.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
