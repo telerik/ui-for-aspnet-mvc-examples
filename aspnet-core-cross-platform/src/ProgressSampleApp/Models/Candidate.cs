2a5dc6aeb81be8bc908ef0a35f83ce1f92f9f7fb
eeasss <eeasss@gmail.com>
2017-01-23 18:38:50 +0200

    feat: added aspnet core cross platform sample app

---
+++ aspnet-core-cross-platform/src/ProgressSampleApp/Models/Candidate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ProgressSampleApp.Models
+{
+
+  public class Candidate
+  {
+
+    public string Name { get; set; }
+
+    public string Party { get; set; }
+
+    public StateResult[] Results { get; set; }
+
+    public long TotalVotes { get
+      {
+        return Results.Sum(s => s.TotalVotes);
+      } }
+
+    public int ElectoralVotes { get
+      {
+        return Results.Sum(s => s.ElectoralVotes);
+      } }
+
+  }
+
+
+}
