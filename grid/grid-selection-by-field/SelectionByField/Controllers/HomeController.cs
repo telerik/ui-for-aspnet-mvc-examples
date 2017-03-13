bb3c227156bd13143a4c395e6e364d14515b8f3a
Angel Petrov <angel.petrov@telerik.com>
2017-03-13 16:18:02 +0200

    Add example for selection by field in grid

---
+++ grid/grid-selection-by-field/SelectionByField/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using SelectionPersistence.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SelectionPersistence.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static List<Product> products = new List<Product>();
+        public ActionResult Index()
+        {
+            if(products.Count == 0)
+            {
+                for (int i = 0; i < 200; i++)
+                {
+                    products.Add(new Product()
+                    {
+                        ProductID = i,
+                        ProductName = "ProductName" + i.ToString(),
+                        UnitPrice = i * 3.14,
+                        UnitsInStock = i * 5,
+                        Discontinued = (i % 2 == 0) ? true : false
+                    });
+                }
+            }
+
+            return View(AreAllSelected());
+        }
+
+        public bool AreAllSelected()
+        {
+            var selectAll = true;
+            foreach (var product in products)
+            {
+                if (product.Discontinued == false)
+                {
+                    selectAll = false;
+                    break;
+                }
+            }
+            return selectAll;
+        }
+
+        public ActionResult Get_Products([DataSourceRequest] DataSourceRequest dsRequest)
+        {
+            var result = products.ToDataSourceResult(dsRequest);
+            return Json(result);
+        }
+
+        public ActionResult Select_Product(Product product)
+        {
+            var toUpdate = products.FirstOrDefault(p => p.ProductID == product.ProductID);
+            toUpdate.Discontinued = product.Discontinued;
+            return Json(AreAllSelected());
+        }
+
+        public ActionResult Select_Products(bool checkAll)
+        {
+            foreach (var product in products)
+            {
+                product.Discontinued = checkAll;
+            }
+            return new EmptyResult();
+        }
+    }
+}
