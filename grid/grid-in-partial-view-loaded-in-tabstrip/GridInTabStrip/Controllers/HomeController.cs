9fa429a53b5643d528987ecc20a70a6f67080b51
ivan.danchev <ifujin@gmail.com>
2017-10-23 09:56:12 +0300

    add(grid): Grid in Partial View loaded in TabStrip example

---
+++ grid/grid-in-partial-view-loaded-in-tabstrip/GridInTabStrip/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using GridInTabStrip.Models;
+using System.Linq;
+using System.Web.Mvc;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+
+namespace GridInTabStrip.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Displays the initial view ~/Views/Home/Index.cshtml
+        /// </summary>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Provides data for the grid. 
+        /// </summary>
+        [HttpPost]
+        public ActionResult Select([DataSourceRequest]DataSourceRequest request)
+        {
+            // Creating dummy data to bind the grid 
+            var data = Enumerable.Range(1, 100)
+                                 .Select(index => new Customer
+                                 {
+                                     ID = index,
+                                     Name = "Customer #" + index
+                                 });
+
+            return Json(data.ToDataSourceResult(request));
+        }
+
+        /// <summary>
+        /// Displays the partial view that contains the grid - ~/Views/Home/Grid.cshtml
+        /// Will be requested via ajax by the tabstrip when the user clicks the 'Grid' tab.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Grid()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ grid/grid-in-partial-view-loaded-in-tabstrip/GridInTabStrip/Models/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace GridInTabStrip.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
