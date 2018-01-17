48d0c684de6023bc4586abc720f659360c30c86d
Viktor Tachev <vtachev@progress.com>
2018-01-17 11:46:58 +0200

    add grid odata example

---
+++ grid/grid-odata-example/OdataSample/OdataSample/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using OdataSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdataSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+
+
+    }
+}
---
+++ grid/grid-odata-example/OdataSample/OdataSample/Models/ProductDTO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace OdataSample.Models
+{
+    [DataContract(Name = "Products")]
+    public class ProductDTO
+    {
+        [Key]
+        [DataMember]
+        public int ProductID { get; set; }
+        [DataMember]
+        public string ProductName { get; set; }
+        [DataMember]
+        public int? SupplierID { get; set; }
+        [DataMember]
+        public int? CategoryID { get; set; }
+        [DataMember]
+        public string QuantityPerUnit { get; set; }
+        [DataMember]
+        [DataType(DataType.Currency)]
+        public decimal? UnitPrice { get; set; }
+        [DataMember]
+        [DataType("Integer")]
+        public short? UnitsInStock { get; set; }
+        [DataMember]
+        [DataType("Integer")]
+        public short? UnitsOnOrder { get; set; }
+        [DataMember]
+        public short? ReorderLevel { get; set; }
+        [DataMember]
+        public bool Discontinued { get; set; }
+    }
+}
