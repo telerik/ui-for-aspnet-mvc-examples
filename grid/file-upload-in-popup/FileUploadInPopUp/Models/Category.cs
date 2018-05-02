3084566c06e5af2fc106dc10b30d5ccd53aec2fa
Viktor Tachev <vtachev@progress.com>
2018-05-02 15:29:25 +0300

    chore: add upload in grid popup sample

---
+++ grid/file-upload-in-popup/FileUploadInPopUp/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FileUploadInPopUp.Models
+{
+    public class Category
+    {
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [UIHint("FileUpload")]
+        [Required]
+        public string ImageUrl { get; set; }
+    }
+}
