79348048121ee6e643b9d3a0fa2a5ca6981caecd
Juveniel <dimitar.kalaydzhievv@gmail.com>
2018-06-15 08:17:35 +0300

    chore: add treeview load selected item async project

---
+++ treeview/ExpandSelectedItemAsync/ExpandSelectedItemAsync/Models/HierarchicalViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace TelerikMvcAppTreeViewLoadItemsAsync.Models
+{
+    public class HierarchicalViewModel
+    {
+        public int ID { get; set; }
+
+        public int? ParentID { get; set; }
+
+        public string Name { get; set; }
+
+        public bool Expanded { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public bool HasChildren { get; set; }
+
+        public IEnumerable<object> Children { get; set; }
+    }
+}
