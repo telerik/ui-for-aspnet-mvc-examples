73304cc42299b26db532cd9fcd9c29aa2de4c68f
Viktor Tachev <vtachev@progress.com>
2018-10-25 16:48:24 +0300

    chore: add customize delete confirmation example telerik/kendo#9017

---
+++ grid/customize-confirmation-window/KendoUIMVC5/Models/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KendoUIMVC5.Models
+{
+    public class Test
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
