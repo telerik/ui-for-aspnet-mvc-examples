c1b62613ca894d7c86638bc5614ebfc425ef1c5b
Tsvetomir Milchev <tmilchev@progress.com>
2019-03-12 15:38:23 +0200

    chore: add sample for binding select column to model field

---
+++ grid/bind-select-column-to-model-field/src/Models/ProductViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KendoGridBindSelectColumn.Models
+{
+    public class ProductViewModel
+    {
+        public int ProductID { get; set; }
+        // The ProductName property is required
+        [Required]
+        public string ProductName { get; set; }
+        // Use the Integer editor template for the UnitsInStock property
+        [UIHint("Integer")]
+        public short? UnitsInStock { get; set; }
+        public bool Discontinued { get; set; }
+    }
+}
