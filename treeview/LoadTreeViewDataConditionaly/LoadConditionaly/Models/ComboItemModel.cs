9605476e16efd6869fe21e42876347ae57c1481f
veselints <veselintsvetanov@gmail.com>
2019-06-11 12:51:23 +0300

    docs: add TreeView data conditional loading example

---
+++ treeview/LoadTreeViewDataConditionaly/LoadConditionaly/Models/ComboItemModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoadConditionaly.Models
+{
+	public class ComboItemModel
+	{
+		public int ID { get; set; }
+
+		public string Name { get; set; }
+	}
+}
