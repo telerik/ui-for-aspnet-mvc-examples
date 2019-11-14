14a9092b87895d5ebaf1a0732c059b8f77491426
aevangel <aleksandar.evangelatov@progress.com>
2019-11-14 08:53:16 +0200

    Add MVC ClientThemeChange example telerik/kendo#9651

---
+++ styles-and-layout/ChangingThemesOnTheClient/ChangingThemesOnTheClient/Models/Theme.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChangingThemesOnTheClient.Models
+{
+    public class Theme
+    {
+        public string ThemeId { get; set; }
+        public string Name { get; set; }
+    }
+}
