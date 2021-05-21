e96696ff6b519de53c01d34aaf331b97bda4d90e
eyupyusein <yusein@progress.com>
2021-05-21 16:17:08 +0300

    chore(grid): add cancellation token sample (#36)
    * chore(grid): create placeholder
    
    * chore(grid): add cancellation token sample

---
+++ grid/grid-cancellationtoken/KendoGridCancellationToken/Models/OrderViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace KendoGridCancellationToken.Models
+{
+    public class OrderViewModel
+    {
+        [ReadOnly(true)]
+        public int OrderID
+        {
+            get;
+            set;
+        }
+
+        public string CustomerID { get; set; }
+
+        public string ContactName
+        {
+            get;
+            set;
+        }
+
+        public decimal? Freight
+        {
+            get;
+            set;
+        }
+
+        public string ShipAddress
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public DateTime? OrderDate
+        {
+            get;
+            set;
+        }
+
+        public DateTime? ShippedDate
+        {
+            get;
+            set;
+        }
+
+        public string ShipCountry
+        {
+            get;
+            set;
+        }
+
+        public string ShipCity
+        {
+            get;
+            set;
+        }
+
+        public string ShipName
+        {
+            get;
+            set;
+        }
+
+        public int? EmployeeID
+        {
+            get;
+            set;
+        }
+    }
+}
