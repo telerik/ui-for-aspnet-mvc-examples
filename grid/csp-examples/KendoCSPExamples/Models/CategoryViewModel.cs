d5a78bbaad95430b37a65fa677e169fd56d2b9be
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2023-09-19 19:22:56 +0300

    chore: add Grid CSP examples

---
+++ grid/csp-examples/KendoCSPExamples/Models/CategoryViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace KendoCSPExamples.Models
+{
+    public class CategoryViewModel
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
---
+++ grid/csp-examples/KendoCSPExamples/Models/CountryViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace KendoCSPExamples.Models
+{
+    public class CountryViewModel
+    {
+        public int CountryID { get; set; }
+        public string CountryNameShort { get; set; }
+        public string CountryNameLong { get; set; }
+    }
+}
+
