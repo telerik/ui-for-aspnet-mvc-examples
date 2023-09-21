100ab94a50db2208a3f6cbbd49c34aa8f601b2fa
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2023-09-21 15:42:35 +0300

    apply-fixings

--- grid/csp-examples/KendoCSPExamples/Global.asax.cs
+++ grid/csp-examples/KendoCSPExamples/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Kendo.Mvc.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +17,11 @@ namespace KendoCSPExamples
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            KendoMvc.Setup(x =>
+            {
+                x.DeferToScriptFiles = true;
+            });
         }
+       
     }
 }
