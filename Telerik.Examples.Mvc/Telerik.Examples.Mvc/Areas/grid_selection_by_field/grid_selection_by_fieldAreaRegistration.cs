a4dbccb20608c0eeba5fcff3f3e741ee4945d486
eyupyusein <yusein@progress.com>
2024-05-30 17:51:07 +0300

    use latest trial lite nuget version

---
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/Areas/grid_selection_by_field/grid_selection_by_fieldAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Telerik.Examples.Mvc.Areas.grid_selection_by_field
+{
+    public class grid_selection_by_fieldAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "grid_selection_by_field";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "grid_selection_by_field_default",
+                "grid_selection_by_field/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
