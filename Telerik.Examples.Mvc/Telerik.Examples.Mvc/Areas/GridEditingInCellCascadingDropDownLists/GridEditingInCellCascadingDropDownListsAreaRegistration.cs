6baa2344e679e9f3043e21091905975b39b2f482
IvanDanchev <ifujin@gmail.com>
2024-06-18 19:33:52 +0300

    chore(examples): add examples to separate areas vol2

---
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/Areas/GridEditingInCellCascadingDropDownLists/GridEditingInCellCascadingDropDownListsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Telerik.Examples.Mvc.Areas.GridEditingInCellCascadingDropDownLists
+{
+    public class GridEditingInCellCascadingDropDownListsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "GridEditingInCellCascadingDropDownLists";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "GridEditingInCellCascadingDropDownLists_default",
+                "GridEditingInCellCascadingDropDownLists/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/Areas/GridEditingInLineNullableBoolean/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Telerik.Examples.Mvc.Areas.GridEditingInLineNullableBoolean.Models;
+
+namespace Telerik.Examples.Mvc.Areas.GridEditingInLineNullableBoolean.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: GridEditingInLineNullableBoolean/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public static IList<InLineEditingProduct> Products
+        {
+            get;
+            set;
+        }
+
+        static HomeController()
+        {
+            var random = new Random();
+
+            Products = Enumerable.Range(0, 20).Select(index => new InLineEditingProduct
+            {
+                ProductID = index + 1,
+                ProductName = "Product " + index,
+                UnitsInStock = random.Next(20, 200),
+                Available = index % 3 == 0 ? (bool?)null : index % 2 == 0
+            }).ToList();
+        }
+
+
+        public ActionResult Read([DataSourceRequest] DataSourceRequest request)
+        {
+            return Json(Products.ToDataSourceResult(request));
+        }
+
+        public ActionResult Update([DataSourceRequest] DataSourceRequest request, InLineEditingProduct product)
+        {
+            if (ModelState.IsValid)
+            {
+                var dataItem = Products.Where(p => p.ProductID == product.ProductID).FirstOrDefault();
+
+                dataItem.ProductID = product.ProductID;
+                dataItem.ProductName = product.ProductName;
+                dataItem.UnitsInStock = product.UnitsInStock;
+                dataItem.Available = product.Available;
+            }
+
+            // Return the updated entities as well as any validation errors.
+            return Json(new[] { product }.ToDataSourceResult(request, ModelState));
+        }
+    }
+}
