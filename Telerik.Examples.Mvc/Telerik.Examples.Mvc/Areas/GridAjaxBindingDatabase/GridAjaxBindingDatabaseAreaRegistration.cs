d2ad29b7095695269f21a4be3e943e621d6e2498
IvanDanchev <ifujin@gmail.com>
2024-06-25 13:23:07 +0300

    chore(examples): add database and configure connection string

---
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/Areas/GridAjaxBindingDatabase/GridAjaxBindingDatabaseAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Telerik.Examples.Mvc.Areas.GridAjaxBindingDatabase
+{
+    public class GridAjaxBindingDatabaseAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "GridAjaxBindingDatabase";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "GridAjaxBindingDatabase_default",
+                "GridAjaxBindingDatabase/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
