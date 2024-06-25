e1ba4c12391623cb603874a80b0407cee61aec04
IvanDanchev <ifujin@gmail.com>
2024-06-25 13:23:07 +0300

    chore(examples): add database and configure connection string

---
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/Areas/GridAjaxBindingDatabase/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Telerik.Examples.Mvc.Areas.GridAjaxBindingDatabase.Models;
+
+namespace Telerik.Examples.Mvc.Areas.GridAjaxBindingDatabase.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: GridAjaxBindingDatabase/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Products_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            using (var sample = new SampleEntities())
+            {
+                IQueryable<Product> products = sample.Products;
+                DataSourceResult result = products.ToDataSourceResult(request);
+                return Json(result);
+            }
+        }
+    }
+}
