484d622060abf19ee3a9061e03f43f5b24a84b82
eyupyusein <yusein@progress.com>
2024-08-05 16:08:34 +0300

    Revert "chore: remove old grid common area folder"
    This reverts commit 772ef2114496119b74f71b285ef2555d6f381824.

---
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/Areas/Grid/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Telerik.Examples.Mvc.Areas.Grid.Models
+{
+	public class Product
+	{
+		public int ProductId { get; set; }
+		public string ProductName { get; set; }
+		public int VendorId { get; set; }
+        public Category Category { get; set; }
+    }
+}
