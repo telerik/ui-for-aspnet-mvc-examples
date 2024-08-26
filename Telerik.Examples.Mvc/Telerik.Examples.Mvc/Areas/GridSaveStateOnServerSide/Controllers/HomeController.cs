1fdf84cd427fdecdac6ce8488740e2a0d0f6400f
eyupyusein <yusein@progress.com>
2024-08-26 09:48:26 +0300

    chore: add the save on server grid sample telerik/kendo#19711

---
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/Areas/GridSaveStateOnServerSide/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Telerik.Examples.Mvc.Areas.GridSaveStateOnServerSide.Models;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Telerik.Examples.Mvc.Areas.GridSaveStateOnServerSide.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult Read([DataSourceRequest] DataSourceRequest request)
+        {
+            var products = Enumerable.Range(1, 100).Select(index => new Product
+            {
+                ProductID = index,
+                ProductName = index + " Product Name"
+            });
+
+            return Json(products.ToDataSourceResult(request));
+        }
+
+        public ActionResult Save(string data)
+        {
+            Session["data"] = data;
+
+            return new EmptyResult();
+        }
+
+        public ActionResult Load()
+        {
+            return Json(Session["data"], JsonRequestBehavior.AllowGet);
+        }
+    }
+}
