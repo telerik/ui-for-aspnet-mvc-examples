63d3b033dbed4c9c2ad6a829ae5698e30e91f8ea
eyupyusein <yusein@progress.com>
2024-08-26 11:45:14 +0300

    chore: migrate old code libraries part 1

---
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/Areas/CdnFallback/CdnFallbackAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Telerik.Examples.Mvc.Areas.CdnFallback
+{
+    public class CdnFallbackAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "CdnFallback";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "CdnFallback_default",
+                "CdnFallback/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
