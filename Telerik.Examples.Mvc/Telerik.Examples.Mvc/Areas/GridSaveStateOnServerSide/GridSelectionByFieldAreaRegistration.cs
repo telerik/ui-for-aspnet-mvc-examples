b202aaf5fd0967b6ce3ee7fc8e609d5a1c7c0103
eyupyusein <yusein@progress.com>
2024-08-26 11:29:31 +0300

    chore: add the save on server grid sample

---
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/Areas/GridSaveStateOnServerSide/GridSelectionByFieldAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Telerik.Examples.Mvc.Areas.GridSaveStateOnServerSide
+{
+    public class EditorImportExportKendoWidgetAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "GridSaveStateOnServerSide";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "GridSaveStateOnServerSide_default",
+                "GridSaveStateOnServerSide/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
