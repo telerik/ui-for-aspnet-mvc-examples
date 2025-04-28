526b09ef9b6e2e13958ab440d3dbeb7baa015d10
ivaylo-milanov <imilanov@progress.com>
2025-04-28 17:36:08 +0300

    chore(docs): add chat peer-to-peer demo telerik/kendo#19688

---
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/Areas/ChatPeerToPeer/ChatPeerToPeerAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Telerik.Examples.Mvc.Areas.ChatPeerToPeer
+{
+    public class ChatPeerToPeerAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "ChatPeerToPeer";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "ChatPeerToPeer_default",
+                "ChatPeerToPeer/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
