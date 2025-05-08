4d0c8cf1cc365d3b64e0280b9ac37aade1d56490
Ivaylo Milanov <imilanov@progress.com>
2025-05-08 16:23:16 +0300

    chore(docs): add chat peer-to-peer demo telerik/kendo#19688 (#532)

---
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/Areas/ChatPeerToPeer/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace Telerik.Examples.Mvc.Areas.ChatPeerToPeer.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(object sender, string message)
+        {
+            Clients.Others.broadcastMessage(sender, message);
+        }
+        public void SendTyping(object sender)
+        {
+            Clients.Others.typing(sender);
+        }
+    }
+}
